756b1a8f1a92bfc508fb5972192151bb79052994
adriansousapoza <adrian.sousapoza@gmail.com>
2025-03-13 11:37:11 +0100

    Update package structure and export symbols in __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="torchgmm",
-    version="0.1.0",
+    version="0.1.1",
     author="Your Name",
     author_email="adrian.sousapoza@gmail.com",
     description="A Gaussian Mixture Model (GMM) based on Expectation-Maximisation (EM) implemented in PyTorch",
